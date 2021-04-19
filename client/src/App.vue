52753c1a8e21d729c250af5e7e9a85ae7b0ed616
mamapam <mamapam.dev@gmail.com>
2021-04-19 11:46:24 -0400

    Added header bar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,3 +1,33 @@
 <template>
-  <h1>Hello World</h1>
+  <the-header></the-header>
 </template>
+
+<script>
+import TheHeader from './components/layout/TheHeader.vue';
+
+export default {
+  components: {
+    TheHeader,
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=EB+Garamond:wght@400;500;600;700&display=swap');
+@font-face {
+  font-family: 'Urdu';
+  src: local('Urdu'), url('./assets/fonts/Jameel_1.ttf') format('truetype');
+}
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'EB Garamond', serif;
+}
+
+body {
+  margin: 0;
+}
+</style>

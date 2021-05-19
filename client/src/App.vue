426931e1924f6170071257502e0c136b6a0a040f
mamapam <mamapam.dev@gmail.com>
2021-05-19 14:09:52 -0400

    Setup header

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,3 +1,40 @@
 <template>
-  <h1>Hello World</h1>
+  <the-header></the-header>
 </template>
+
+<script>
+import TheHeader from './components/layout/TheHeader.vue';
+
+export default {
+  components: {
+    TheHeader,
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=EB+Garamond:wght@400;500;600;700&display=swap');
+
+@font-face {
+  font-family: 'MeQuran';
+  src: local('MeQuran'), url('./assets/fonts/me_quran_2.ttf') format('truetype');
+}
+
+@font-face {
+  font-family: 'Urdu';
+  src: local('Urdu'), url('./assets/fonts/Jameel_1.ttf') format('truetype');
+}
+
+html {
+  font-family: 'EB Garamond', serif;
+}
+
+body,
+p,
+a,
+ul,
+li {
+  padding: 0;
+  margin: 0;
+}
+</style>
---
+++ client/src/components/layout/TheHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav>
+    <h1>Quran Duas</h1>
+    <div>
+      <img src="../../assets/images/bismillah.png" alt="Bismillah" />
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  width: 100%;
+  color: white;
+  background-color: #114b5f;
+  text-align: center;
+}
+
+h1 {
+  margin: 0;
+  padding-top: 10px;
+  font-size: 1.4em;
+}
+
+div {
+  width: 160px;
+  height: 70px;
+  margin: 0 auto;
+}
+
+img {
+  width: 100%;
+  height: 100%;
+}
+</style>
